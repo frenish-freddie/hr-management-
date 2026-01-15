2b954da1a42fd187598881e41c4bfc5b5d610054
Frenish Freddie <frenishfreddiearattu@gmail.com>
2026-01-15 21:00:57 +0530

    Backend updates

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",   # React
-        "http://127.0.0.1:3000"
+        "http://localhost:5173",   # React
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],  # GET, POST, PUT, DELETE
